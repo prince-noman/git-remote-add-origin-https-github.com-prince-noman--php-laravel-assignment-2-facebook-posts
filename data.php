2b3b35504c5013addedaf2a286bb69cf17b3cba8
prince-noman <prince1000bd@gmail.com>
2022-03-27 21:27:34 +0600

    file uploaded

---
+++ data.php
@@ -0,0 +1,49 @@
+<?php
+
+$people = [
+    [
+        'name'              => 'Shafin Ahmed',
+        'time'              => '2 mins ago',
+        'photo'             => 'https://cdn.powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png',
+        'content'           => 'We grow fearless when we do the things we fear.',
+        'featured_image'    => 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
+        'like'              => 150,
+        'comment'           => 13
+    ],
+    [
+        'name'              => 'Rakin Hossian',
+        'time'              => '5 mins ago',
+        'photo'             => 'https://lycheehotel.hk/wp-content/uploads/2018/09/team-member-3.jpg',
+        'content'           => 'And, when you want something, all the universe conspires in helping you to achieve it.',
+        'featured_image'    => 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg',
+        'like'              => 170,
+        'comment'           => 5
+    ],
+    [
+        'name'              => 'Helal Khan',
+        'time'              => '9 mins ago',
+        'photo'             => 'https://cdn.powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-9.png',
+        'content'           => 'When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one which has been opened for us.',
+        'featured_image'    => 'https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111-1500x1000.jpg',
+        'like'              => 250,
+        'comment'           => 19
+    ],
+    [
+        'name'              => 'Mickel Josh',
+        'time'              => '14 mins ago',
+        'photo'             => 'https://unitedthemes.com/wp-content/uploads/2018/09/team2.jpg',
+        'content'           => 'If you hear a voice within you say "you cannot paint", then by all means paint and that voice will be silenced.',
+        'featured_image'    => 'https://img.etimg.com/thumb/msid-68721417,width-650,imgsize-1016106,,resizemode-4,quality-100/nature1_gettyimages.jpg',
+        'like'              => 330,
+        'comment'           => 29
+    ],
+    [
+        'name'              => 'Jahangir Alam',
+        'time'              => '30 mins ago',
+        'photo'             => 'http://prokritipolli.com/wp-content/uploads/2017/04/team1.jpg',
+        'content'           => 'Love is an untamed force. When we try to control it, it destroys us. When we try to imprison it, it enslaves us. When we try to understand it, it leaves us feeling lost and confused.',
+        'featured_image'    => 'https://wallpapercave.com/wp/wp2759938.jpg',
+        'like'              => 33,
+        'comment'           => 23
+    ]
+];
